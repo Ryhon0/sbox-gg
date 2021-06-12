474e0c4028d64d1f8211d74359bd84a9d9a3ed81
Ryhon <ryhon@ryhn.link>
2021-06-12 21:49:32 +0200

    Split Player into partial classes

---
+++ code/Player/Player.Inventory.cs
@@ -0,0 +1,53 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+public partial class Player
+{
+	public Player()
+	{
+		Inventory = new GGInventory( this );
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		if ( other is BaseWeapon ) return;
+
+		base.StartTouch( other );
+	}
+}
+
+partial class GGInventory : BaseInventory
+{
+	public GGInventory( Player player ) : base( player )
+	{
+
+	}
+
+	public override bool Add( Entity ent, bool makeActive = false )
+	{
+		var player = Owner as Player;
+		var weapon = ent as Weapon;
+
+		if ( weapon != null && IsCarryingType( ent.GetType() ) )
+		{
+			var ammo = weapon.AmmoClip;
+
+			// Despawn it
+			ent.Delete();
+			return false;
+		}
+
+		if ( weapon != null )
+		{
+			Sound.FromWorld( "dm.pickup_weapon", ent.Position );
+		}
+
+		return base.Add( ent, makeActive );
+	}
+
+	public bool IsCarryingType( Type t )
+	{
+		return List.Any( x => x.GetType() == t );
+	}
+}
---
+++ code/Player/Player.cs
@@ -0,0 +1,77 @@
+﻿using Sandbox;
+
+public partial class Player : Sandbox.Player
+{
+	public override void Respawn()
+	{
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		Controller = new WalkController();
+		Animator = new StandardPlayerAnimator();
+		Camera = new FirstPersonCamera();
+
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		(Game.Current as Game).RequestWeapon( this );
+
+		base.Respawn();
+	}
+
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+
+		SimulateActiveChild( cl, ActiveChild );
+	}
+	DamageInfo LastDamage;
+	public override void TakeDamage( DamageInfo info )
+	{
+		LastDamage = info;
+
+		// hack - hitbox 0 is head
+		// we should be able to get this from somewhere
+		if ( info.HitboxIndex == 0 )
+		{
+			info.Damage *= 2.0f;
+		}
+
+		base.TakeDamage( info );
+
+		if ( info.Attacker is Player attacker && attacker != this )
+		{
+			// Note - sending this only to the attacker!
+			attacker.DidDamage( To.Single( attacker ), info.Position, info.Damage, Health.LerpInverse( 100, 0 ) );
+
+			TookDamage( To.Single( this ), info.Weapon.IsValid() ? info.Weapon.Position : info.Attacker.Position );
+		}
+	}
+	[ClientRpc]
+	public void TookDamage( Vector3 pos )
+	{
+		//DebugOverlay.Sphere( pos, 5.0f, Color.Red, false, 50.0f );
+
+		DamageIndicator.Current?.OnHit( pos );
+	}
+	[ClientRpc]
+	public void DidDamage( Vector3 pos, float amount, float healthinv )
+	{
+		Sound.FromScreen( "dm.ui_attacker" )
+			.SetPitch( 1 + healthinv * 1 );
+
+		HitIndicator.Current?.OnHit( pos, amount );
+	}
+}
+
+public class KillArgs
+{
+	public Entity Killer;
+	public Player Killed;
+	public KillArgs( Entity killer, Player killed )
+	{
+		Killer = killer;
+		Killed = killed;
+	}
+}
