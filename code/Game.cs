c431395c3996fe5d0f65fc556d140f1fbc4209c8
Ryhon <ryhon@ryhn.link>
2021-06-12 14:18:04 +0200

    Initial commit

---
+++ code/Game.cs
@@ -0,0 +1,34 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+[Library( "gg" )]
+public partial class Game : Sandbox.Game
+{
+	public Game()
+	{
+		if ( IsServer )
+		{
+			new MinimalHudEntity();
+		}
+
+		if ( IsClient )
+		{
+
+		}
+	}
+
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var player = new Player();
+		client.Pawn = player;
+
+		player.Respawn();
+	}
+}
+
---
+++ code/Hud.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox.UI;
+
+public partial class MinimalHudEntity : Sandbox.HudEntity<RootPanel>
+{
+	public MinimalHudEntity()
+	{
+		if ( IsClient )
+		{
+
+		}
+	}
+}
