96b8031479fca615b9859f5512770f578802fbf8
Ryhon <ryhon@ryhn.link>
2021-06-12 21:48:43 +0200

    Gun Game logic

--- code/Game.cs
+++ code/Game.cs
@@ -1,9 +1,5 @@
-﻿
-using Sandbox;
-using Sandbox.UI.Construct;
-using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using Sandbox;
+using System.Collections.Generic;
 
 [Library( "gg" )]
 public partial class Game : Sandbox.Game
@@ -24,11 +20,59 @@ public partial class Game : Sandbox.Game
 	public override void ClientJoined( Client client )
 	{
 		base.ClientJoined( client );
-
 		var player = new Player();
 		client.Pawn = player;
-
+		client.SetScore( "rank", 0 );
 		player.Respawn();
 	}
-}
 
+	List<string> Weapons = new List<string>()
+	{
+		"smg",
+		"pistol",
+		"pump",
+		"pipe",
+		"xbow",
+		"knife",
+	};
+
+	[Event( "player_killed" )]
+	void PlayerKilled( KillArgs args )
+	{
+		if ( args.Killer is Player p )
+		{
+			var owner = p.GetClientOwner();
+
+			GivePoint( owner );
+		}
+	}
+
+	void GivePoint( Client c )
+	{
+		var rank = c.GetScore<int>( "rank", 0 ) + 1;
+		c.SetScore( "rank", rank );
+
+		GiveWeapon( c.Pawn as Player, GetWeapon( rank ) );
+	}
+
+	string GetWeapon( int rank )
+	{
+		var i = rank.Wrap( 0, Weapons.Count );
+		return Weapons[i];
+	}
+
+	void GiveWeapon( Player p, string weapon )
+	{
+		var d = p.Inventory.DropActive();
+		d?.Delete();
+
+		var w = Entity.Create( weapon );
+		p.Inventory.Add( w );
+		p.ActiveChild = w;
+	}
+
+	public void RequestWeapon( Player p )
+	{
+		GiveWeapon( p, GetWeapon( p.GetClientOwner().GetScore<int>( "rank", 0 ) ) );
+	}
+}
