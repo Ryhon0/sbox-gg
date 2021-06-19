910d6991ac4b0143632c6c61d443f6e8ae63563a
Ryhon <ryhon@ryhn.link>
2021-06-18 17:49:16 -0700

    Organize properties in Weapon.cs

--- code/Weapons/Base/Weapon.cs
+++ code/Weapons/Base/Weapon.cs
@@ -19,6 +19,7 @@ public enum CrosshairType
 
 public partial class Weapon : BaseWeapon
 {
+	// Networked variables
 	[Net, Predicted]
 	public int AmmoClip { get; set; }
 	[Net, Predicted]
@@ -28,28 +29,36 @@ public partial class Weapon : BaseWeapon
 	[Net, Predicted]
 	public TimeSince TimeSinceDeployed { get; set; }
 
+	// Ammo
 	public virtual int ClipSize => 1;
 	public virtual bool ReloadMagazine => true;
 	public virtual float ReloadTime => 2f;
 
+	// Projectile specific
 	public virtual string Projectile => null;
 	public virtual float ProjectileSpeed => 1000;
 
+	// Hitscan specific
+	public virtual float BulletSize => IsMelee ? 50f : 1f;
+	public virtual float Range => IsMelee ? 75 : 5000;
+
+	// Stats
+	public virtual bool IsMelee => false;
+	public virtual float Force => 0.5f;
 	public virtual float Damage => 10f;
-	public virtual int RPM => 600;
-	public virtual float AttackInterval => 60f / RPM;
 	public virtual int BulletsPerShot => 1;
 	public virtual bool IsAutomatic => true;
-	public virtual bool IsMelee => false;
+	public virtual int BulletsPerShot => 1;
 	public virtual float Spread => 0.1f;
-	public virtual float Force => 0.5f;
+	public virtual int RPM => 600;
+	public virtual float AttackInterval => 60f / RPM;
+	public virtual float DeployTime => .75f;
 
-	public virtual float DeployTime => 0.25f;
 
-	public virtual string ShootShound => "rust_pistol.shoot";
-	public virtual CrosshairType CrosshairType => CrosshairType.Dot;
-
+	// Audio/Visual
 	public virtual int HoldType => 1;
+	public virtual CrosshairType CrosshairType => CrosshairType.Dot;
+	public virtual string ShootShound => "rust_pistol.shoot";
 	public virtual string WorldModelPath => "weapons/rust_pistol/rust_pistol.vmdl";
 	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
 
