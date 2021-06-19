406b0b591438c9d805b8862545f9e84c2000f821
Ryhon <ryhon@ryhn.link>
2021-06-19 15:40:20 +0200

    Make projectiles act as debris

--- code/Weapons/Base/Projectile.cs
+++ code/Weapons/Base/Projectile.cs
@@ -22,6 +22,7 @@ public class Projectile : BasePhysics
 		SetModel( ModelPath );
 
 		CollisionGroup = CollisionGroup.Weapon;
+		SetInteractsAs( CollisionLayer.Debris );
 	}
 
 	protected override ModelPropData GetModelPropData()
