27a3354a235bf365c82158ce2d875488df6bd58a
Ryhon <ryhon@ryhn.link>
2021-06-29 12:29:11 +0200

    Don't use SceneCapture

--- code/UI/GunGame/GunGameHUD.Player.cs
+++ code/UI/GunGame/GunGameHUD.Player.cs
@@ -6,61 +6,33 @@ using System.Linq;
 partial class GunGameHUD
 {
 	private Angles CamAngles;
-	static SceneCapture sceneCapture;
 	private AnimSceneObject playerPreview;
 	private AnimSceneObject playerCostumePreview;
 	private float startTime;
 
-	public Image heroImage { get; set; }
+	public Scene heroScene { get; set; }
 	public int WinnerID;
 
-	[Event.Hotload]
-	void OnHotload()
-	{
-		LoadWorld();
-	}
-
-	private void DeleteScene()
-	{
-		sceneCapture?.Delete();
-		sceneCapture = null;
-	}
-
+	public Panel HeroPortrait { get; set; }
 	private void LoadWorld()
 	{
-		var par = heroImage.Parent;
-		heroImage.Delete();
-		heroImage = Add.Image( "scene:portrait", "hero-image" );
-		heroImage.Parent = par;
-		DeleteScene();
-
 		using ( SceneWorld.SetCurrent( new SceneWorld() ) )
 		{
 			playerPreview = new AnimSceneObject( Model.Load( "models/citizen/citizen.vmdl" ), Transform.Zero );
 
-			Light.Point( Vector3.Up * 150.0f, 200.0f, Color.White * 5000.0f );
-			Light.Point( Vector3.Up * 100.0f + Vector3.Forward * 100.0f, 200, Color.White * 15000.0f );
-
-			sceneCapture = SceneCapture.Create( "portrait", 512, 512 );
-
-			sceneCapture.AmbientColor = new Color( 0.8f, 0.8f, 0.8f );
-			sceneCapture.SetCamera( Vector3.Up * 100 + CamAngles.Direction * -50, CamAngles, 45 );
+			Light.Point( Vector3.Up * 150.0f + Vector3.Right * 50, 10000, Color.White * 5000.0f );
+			Light.Point( new Vector3( 175, 0, 30 ), 10000, Color.White * 15000.0f );
 
 			// Clothes
 			playerCostumePreview = new AnimSceneObject( Model.Load( "models/clothes/hotdog/hotdog.vmdl" ), Transform.Zero );
 			playerPreview.AddChild( "outfit", playerCostumePreview );
 
-
 			startTime = Time.Now;
+
+			heroScene = HeroPortrait.Add.Scene( SceneWorld.Current, new Vector3( 175, 0, 30 ), CamAngles, 30 );
+			heroScene.Style.Width = 720;
+			heroScene.Style.Height = 720;
 		}
-	}
-
-	public override void OnDeleted()
-	{
-		sceneCapture?.Delete();
-		sceneCapture = null;
-
-		base.OnDeleted();
 	}
 
 	[Event.Tick]
@@ -80,7 +52,6 @@ partial class GunGameHUD
 
 		playerPreview.Update( Time.Now - startTime );
 		playerCostumePreview.Update( Time.Now - startTime );
-		sceneCapture?.SetCamera( new Vector3( 175, 0, 30 ), CamAngles, 30 );
 	}
 
 	void CopyParams( PlayerAnimator from, AnimSceneObject to )
