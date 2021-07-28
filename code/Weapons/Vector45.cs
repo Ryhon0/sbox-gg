d48783d430121f734c52dfa881edef5b5875d950
Ryhon <ryhon@ryhn.link>
2021-07-28 12:29:23 +0200

    Vector placeholder models

--- code/Weapons/Vector45.cs
+++ code/Weapons/Vector45.cs
@@ -10,8 +10,8 @@ public class Vector45 : Weapon
 	public override float Spread => 0.1f;
 	public override int ClipSize => 20;
 	public override string ShootShound => "rust_smg.shoot";
-	public override string WorldModelPath => "weapons/rust_smg/rust_smg.vmdl";
-	public override string ViewModelPath => "weapons/rust_smg/v_rust_smg.vmdl";
+	public override string WorldModelPath => "weapons/vec45/vec45.vmdl";
+	public override string ViewModelPath => "weapons/vec45/v_vec45.vmdl";
 	public override CrosshairType CrosshairType => CrosshairType.Dot;
 	public override HoldType HoldType => HoldType.SMG;
 }
