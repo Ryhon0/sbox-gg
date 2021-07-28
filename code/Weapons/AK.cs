d075134bb2ad9ea0906d3065ef639b1073e0a80b
Ryhon <ryhon@ryhn.link>
2021-07-28 12:25:43 +0200

    AK

---
+++ code/Weapons/AK.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox;
+[Library( "ak" )]
+public class AK : Weapon
+{
+	public override string ViewModelPath => "weapons/ak/v_ak.vmdl";
+	public override string WorldModelPath => "weapons/ak/ak.vmdl";
+	public override int ClipSize => 30;
+	public override int RPM => 650;
+	public override float Damage => 24;
+	public override HoldType HoldType => HoldType.SMG;
+}
